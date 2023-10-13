4fcf338f138ce9d956c5ec2a7e9c777ad89aafd9
haikaljs <haikaljavascript@gmail.com>
2023-10-13 22:31:26 +0800

    Setup db connection

---
+++ inc/dbcon.php
@@ -0,0 +1,15 @@
+<?php 
+    
+    $host = "localshost";
+    $username = "root";
+    $password = "";
+    $dbname = "crud-api-mysqli";
+
+    $conn = mysqli_connect($host, $username, $password, $dbname);
+
+    if(!$conn){
+        die("Connection Failed" . mysqli_connect_error());
+    }
+
+
+?>
